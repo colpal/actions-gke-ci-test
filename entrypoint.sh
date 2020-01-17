b779c43978340e581dba6e5f740201f04ddb1966
Kevin Dukelow <Kevin_Dukelow@colpal.com>
2020-01-17 10:23:52 -0500

    Bug fix: added INPUT_ to variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,24 @@ set -ev
 END=300
 ATTEMPTS=0
 
-echo "$GKE_SERVICE_ACCOUNT" | base64 -d > /tmp/service_account.json
+echo "$INPUT_GKE_SERVICE_ACCOUNT" | base64 -d > /tmp/service_account.json
 
 gcloud auth activate-service-account --key-file=/tmp/service_account.json
 
-gcloud config set project "$GKE_PROJECT"
+gcloud config set project "$INPUT_GKE_PROJECT"
 
-CZONE=$(gcloud container clusters list --format="get(location)" --filter="name=$GKE_CLUSTER_NAME")
+CZONE=$(gcloud container clusters list --format="get(location)" --filter="name=$INPUT_GKE_CLUSTER_NAME")
 
-gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $CZONE --project $GKE_PROJECT
+gcloud container clusters get-credentials $INPUT_GKE_CLUSTER_NAME --zone $CZONE --project $INPUT_GKE_PROJECT
 
 
 
-NAME=$(kubectl create -f $MANIFEST_FILE -o jsonpath='{.spec.template.metadata.labels.job-name}' -n $NAMESPACE)
+NAME=$(kubectl create -f $INPUT_MANIFEST_FILE -o jsonpath='{.spec.template.metadata.labels.job-name}' -n $INPUT_NAMESPACE)
 JOB=job/$NAME
 
 #Make sure Job is active
 echo "Job" $NAME "provisioned, waiting for job to start..."
-until [[ $SECONDS -gt $END ]] || [[ $(kubectl get $JOB -n $NAMESPACE -o jsonpath='{.status.active}') == "1" ]]; do
+until [[ $SECONDS -gt $END ]] || [[ $(kubectl get $JOB -n $INPUT_NAMESPACE -o jsonpath='{.status.active}') == "1" ]]; do
     printf "\rWaiting "$ATTEMPTS"s"
     ((ATTEMPTS=ATTEMPTS+1))
     sleep 1
@@ -29,16 +29,16 @@ done
 
 #Make sure first container is running (act)
 echo "Job created, waiting for pod"
-until [[ $SECONDS -gt $END ]] || [[ $(kubectl get pod -l job-name=$NAME -n $NAMESPACE -o jsonpath='{.items[0].status.phase}') == "Running" ]]; do
-    STATUS=$(kubectl get pods -l job-name=$NAME -n $NAMESPACE -o jsonpath='{.items[0].status.phase}')
+until [[ $SECONDS -gt $END ]] || [[ $(kubectl get pod -l job-name=$NAME -n $INPUT_NAMESPACE -o jsonpath='{.items[0].status.phase}') == "Running" ]]; do
+    STATUS=$(kubectl get pods -l job-name=$NAME -n $INPUT_NAMESPACE -o jsonpath='{.items[0].status.phase}')
     printf "\rPod status: "$STATUS
 done
 
 printf "\n"
 echo "Job started, displaying logs:"
-kubectl logs --follow $JOB -n $NAMESPACE
+kubectl logs --follow $JOB -n $INPUT_NAMESPACE
 
-if [ $(kubectl get $JOB -o jsonpath='{.status.succeeded}' -n $NAMESPACE) == "1" ]
+if [ $(kubectl get $JOB -o jsonpath='{.status.succeeded}' -n $INPUT_NAMESPACE) == "1" ]
 then
     echo "Test job completed successfully"
     exit 0
